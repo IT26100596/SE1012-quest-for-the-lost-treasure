7b67cc779a0c7d0d197d36f371e49882bd398aff
IT26100596 <IT26100596@my.sliit.lk>
2026-07-02 09:02:19 +0530

    Add map generation and basic player movement

--- main.c
+++ main.c
@@ -2,10 +2,237 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAP_SIZE 15
-#define MAX_PLAYERS 3
+#define size_raw 15
+#define size_col 15
+#define num_players 3
+
+char map_grid[size_raw][size_col];
+int hiddenTraps[size_raw][size_col] = {0};
+char road[] = " ";
+char wall[] = "#";
+
+struct player {
+    char name[20];
+    int row;
+    int col;
+    int health;
+    int score;
+    int keys;
+    int player_symbol;
+};
+
+struct player players[num_players];
+int player_count = 1;
+
+
+void initializeMap() {
+
+        for(int i = 0; i < size_raw; i++) {
+
+            for(int j = 0; j < size_col; j++) {
+
+                if(i == 0 || j == 0 || i == size_raw - 1 || j == size_col - 1)
+                map_grid[i][j] = wall[0];
+
+                else
+                map_grid[i][j] = road[0];
+            }
+        }
+    }
+
+void printmap() {
+
+        for(int i = 0; i < size_raw; i++) {
+            
+            for(int j = 0 ; j < size_col; j++) {
+                printf("%c  ", map_grid[i][j]);
+            }
+
+            printf("\n");
+    }
+}
+
+void placeWalls() {
+
+        int wallPlaced = 0;
+        
+        while(wallPlaced < 30) {
+            int row = 1 + rand() % 13;
+            int col = 1 + rand() % 13;
+            
+            if(map_grid[row][col] == road[0]) {
+                map_grid[row][col] = wall[0];
+                wallPlaced++;
+        }
+        }
+    }
+
+void placeTreasures() {
+
+    int treasurePlaced = 0;
+
+    while(treasurePlaced < 12) {
+
+        int row = 1 + rand() % 13;
+        int col = 1 + rand() % 13;
+
+        if(map_grid[row][col] == road[0] ) {
+            map_grid[row][col] = 'T';
+            treasurePlaced++;
+        }
+    }
+}
+
+void placeTraps() {
+    int trapPlaced = 0;
+
+    while(trapPlaced < 10) {
+
+        int row = 1 + rand() % 13;
+        int col = 1 + rand() % 13;
+
+        if(map_grid[row][col] == road[0] && hiddenTraps[row][col] == 0) {
+            hiddenTraps[row][col] = 1;
+            trapPlaced++;
+        }
+        }
+    }
+
+void placeHealthPacks() {
+    int healthPlaced = 0;
+
+    while(healthPlaced < 10) {
+        int row = 1 + rand() % 13;
+        int col = 1 + rand() % 13;
+
+        if(map_grid[row][col] == road[0]) {
+            map_grid[row][col] = 'H';
+            healthPlaced++;
+        }
+    }
+}
+
+void placeKeys() {
+    int keyPlaced = 0;
+
+    while(keyPlaced < 3) {
+        int row = 1 + rand() % 13;
+        int col = 1 + rand() % 13;
+
+        if(map_grid[row][col] == road[0]) {
+            map_grid[row][col] = 'K';
+            keyPlaced++;
+        }
+    }
+}
+
+void placeDoors() {
+    int doorPlaced = 0;
+
+    while(doorPlaced < 3) {
+        int row = 1 + rand() % 13;
+        int col = 1 + rand() % 13;
+
+        if(map_grid[row][col] == road[0]) {
+            map_grid[row][col] = 'D';
+            doorPlaced++;
+        }
+    }
+}
+
+void placePlayers() {
+
+    for(int i = 0; i < player_count; i++) {
+
+        players[i].health = 100;
+        players[i].score = 0;
+        players[i].keys = 0;
+        players[i].player_symbol =  i + '1';
+
+        int placed = 0; // not placed yet
+
+        while(placed == 0) {
+            int row = 1 + rand() % 13;
+            int col = 1 + rand() % 13;
+
+            if(map_grid[row][col] == ' ') {
+                players[i].row = row;
+                players[i].col = col;
+
+                map_grid[row][col] = players[i].player_symbol;
+                placed = 1; // plasing the player
+            }
+        }
+    }
+}
+
+void movePlayer() {
+    char move;
+    int index = 0;
+
+    printf("move(W, S, A, D): ");
+    scanf(" %c", &move);
+
+    int new_row = players[index].row;
+    int new_col = players[index].col;
+
+    if(move == 'W' || move == 'w') {
+        new_row--;
+    }
+
+    else if(move == 'S' || move =='s') {
+        new_row++;
+    }
+
+    else if(move == 'A' || move == 'a') {
+        new_col--;
+    }
+
+    else if(move == 'D' || move == 'd') {
+        new_col++;
+    }
+
+    else {
+        printf("invalide move.\n");
+        return;
+    }
+
+    if(map_grid[new_row][new_col] == '#'){
+        printf("player hit a wall.\n");
+        return;
+    }
+
+
+    map_grid[players[index].row][players[index].col] = road[0];
+
+    players[index].row = new_row;
+    players[index].col = new_col;
+
+    map_grid[players[index].row][players[index].col] = players[index].player_symbol;
+}
+
+
 
 int main() {
-    printf("Quest for the Lost Treasure\n");
+
+    srand(time(NULL));
+
+    initializeMap();
+
+    placeWalls();
+    placeTreasures();
+    placeTraps();
+    placeHealthPacks();
+    placeKeys();
+    placeDoors();
+    placePlayers();
+    
+    printmap();
+    movePlayer();
+    printmap();
+
+
+    
     return 0;
 }
+
