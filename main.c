6971ada26dce8465987edffceae1e6546fe39804
IT26100596 <IT26100596@my.sliit.lk>
2026-07-02 08:53:41 +0530

    Create initial game structure

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAP_SIZE 15
+#define MAX_PLAYERS 3
+
+int main() {
+    printf("Quest for the Lost Treasure\n");
+    return 0;
+}
